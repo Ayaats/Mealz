451d60c7c2c746e0d1fa19e5fd24ab9b2bf91f59
Ayaat <93150311+Ayaats@users.noreply.github.com>
2023-04-13 22:31:37 +0200

    Meals app !!

--- data/src/main/java/com/example/data/repo/MealsRepoImp.kt
+++ data/src/main/java/com/example/data/repo/MealsRepoImp.kt
@@ -4,9 +4,8 @@ import com.example.data.remote.ApiService
 import com.example.domain.entity.CategoryResponse
 import com.example.domain.repo.MealsRepo
 
-class MealsRepoImp (private val apiService: ApiService):ApiService ,MealsRepo {
-    override fun getMeals(): CategoryResponse = apiService.getMeals()
-    override fun getMealsFromRemote(): CategoryResponse =apiService.getMeals()
+class MealsRepoImp(private val apiService: ApiService) : MealsRepo {
+    override suspend fun getMealsFromRemote(): CategoryResponse = apiService.getMeals()
 
 
 }
